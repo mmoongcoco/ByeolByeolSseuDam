066e71118329e506d0991019827a05532899ac9f
Seong-eunji <sejjes07@gmail.com>
2022-11-14 22:05:30 +0900

    new Project

---
+++ src/main/java/com/example/byeolbyeolsseudam/ByeolbyeolsseudamApplication.java
@@ -0,0 +1,13 @@
+package com.example.byeolbyeolsseudam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeolbyeolsseudamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByeolbyeolsseudamApplication.class, args);
+    }
+
+}

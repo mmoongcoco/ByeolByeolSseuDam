74c1638acb7c1175e9195100b4358eb662714325
Jung93 <orijung93@gmail.com>
2022-11-16 22:49:31 +0900

    LoginController, PickupController 수정 login, pick 관련 html, css 추가

--- src/main/java/com/example/byeolbyeolsseudam/controller/login/LoginController.java
+++ src/main/java/com/example/byeolbyeolsseudam/controller/login/LoginController.java
@@ -1,4 +1,25 @@
 package com.example.byeolbyeolsseudam.controller.login;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login/*")
 public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "/app/login/login";
+    }
+
+    @GetMapping("/find_password")
+    public String findPassword(){
+        return "/app/login/find_password";
+    }
+
+    @GetMapping("/find_password_done")
+    public String findPasswordDone(){
+        return "/app/login/find_password_done";
+    }
 }
